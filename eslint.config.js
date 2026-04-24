ed578bf8d881362ccbfb5bb1d7a91cabd69c4e1e
masonchester <110614975+masonchester@users.noreply.github.com>
2026-04-24 19:07:47 -0400

    remove js lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,3 @@
-import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -10,7 +9,6 @@ export default defineConfig([
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
-      js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
